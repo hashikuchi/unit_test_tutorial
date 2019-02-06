faabcc5cf966bcf40570449704b616db629463e6
Kazunori Hassy Hashikuchi <k.hashikuchi@gmail.com>
2019-02-06 17:29:41 +1300

    Implement functions based on the tests

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,5 +1,8 @@
 function validatePassword(password) {
-
+    if (password.length < 6) {
+        return false;
+    }
+    return !!password.match(/^[0-9a-zA-Z]+$/);
 }
 
 module.exports = validatePassword;
